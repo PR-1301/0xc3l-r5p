a0fff6ba080a2fbdacbeaeb80cdccd02fa6b3f13
PR <ponnurajan.1314@gmail.com>
2026-09-17 14:11:11 +0530

    feat: configure CORS origin and enable credentials

--- server/app.js
+++ server/app.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: `https://${process.env.API_URL}`,
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
